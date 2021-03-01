3380f8640b474e3483bd2c8b1771a7125b4a6e08
Reid Wilson <rwilson@tamu.edu>
2021-02-28 22:52:00 -0600

    Implemented Google OAuth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '34762737251-62tavaajeqljpf4v1oii1sjggt7740ue.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = 'RlTdWo5TbEfazg23jEFn0jDh'
+  
 end
