22036c775f2528fc753871ed7c42f57968c5fe25
nickrwann <nickrwann@gmail.com>
2021-03-21 08:15:27 -0500

    Added member model testing (30% coverage)

--- spec/models/member_unit_testing_spec.rb
+++ spec/models/member_unit_testing_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe Member, :type => :model do
             currentCity: "College Station",
             company: "Dell",
             startDate: Time.new,
-            endDate: Time.new
-            # position_ids: ["member_position_ids_1"]
+            endDate: Time.new,
+            position_ids: [Position.last.id]
         )
     }
 
@@ -22,9 +22,49 @@ RSpec.describe Member, :type => :model do
         expect(subject).to be_valid
     end
 
-    # it 'Unit Test: Is not valid without a first name' do
-    #     subject.firstName = nil
-    #     expect(subject).to_not be_valid
-    # end
+    it 'Is not valid without a first name' do
+        subject.firstName = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without an email' do
+        subject.lastName = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without a class year' do
+        subject.classYear = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without an email' do
+        subject.email = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without a phone number' do
+        subject.phone = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without a current city' do
+        subject.currentCity = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without a start date' do
+        subject.startDate = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without an end date' do
+        subject.endDate = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Is not valid without a position held' do
+        subject.position_ids = nil
+        expect(subject).to_not be_valid
+    end
 
 end
