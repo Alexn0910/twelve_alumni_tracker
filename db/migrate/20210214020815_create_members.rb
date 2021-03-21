8d6cd15ad852d6b4d415c72e86f867f0784c018d
terryho618 <43558690+terryho618@users.noreply.github.com>
2021-03-21 16:01:36 -0500

    Added major and other social media

--- db/migrate/20210214020815_create_members.rb
+++ db/migrate/20210214020815_create_members.rb
@@ -4,12 +4,14 @@ class CreateMembers < ActiveRecord::Migration[6.1]
       t.string :firstName
       t.string :lastName
       t.integer :classYear
+      t.string :major
       t.string :email
       t.string :phone
       t.string :socialMediaL
       t.string :socialMediaI
       t.string :socialMediaT
       t.string :socialMediaF
+      t.string :socialMediaO
       t.string :currentCity
       t.string :company
       t.timestamp :startDate
