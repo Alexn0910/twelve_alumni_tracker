97c618a2a98826c9a740f96e0aeeaa82fd43813b
Reid Wilson <rwilson@tamu.edu>
2021-04-15 00:04:32 -0500

    fixed not denying access bug

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,8 +4,18 @@ class Admin < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    return nil unless /@gmail.com || @tamu.edu\z/.match?(email)
+    emailInDatabase = false
+    @members = Member.all.order('id ASC')
+	@members.each do |member|
+		if member.email == email
+			emailInDatabase = true
+		end
+	end
+    #return nil unless /@gmail.com || @tamu.edu\z/.match?(email)
+    return nil unless (emailInDatabase || ("twelvetamu@gmail.com" == email) || ("rwilson@tamu.edu" == email) || ("terryho618@tamu.edu" == email) || ("nguyen.alex@tamu.edu" == email) || ("wann212@tamu.edu" == email) || ("keerthana96@tamu.edu" == email))
 
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
+  
 end
+
